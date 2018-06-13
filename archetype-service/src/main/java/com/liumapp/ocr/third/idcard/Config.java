244dd049bd34b8f7542e6d67bb824515ecb77c0c
liumapp <liumapp.com@gmail.com>
2018-06-13 14:13:01 +0800

    init code by single separation archetype

---
+++ archetype-service/src/main/java/com/liumapp/ocr/third/idcard/Config.java
@@ -0,0 +1,25 @@
+package com.liumapp.ocr.third.idcard;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author liumapp
+ * @file Config.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 6/13/18
+ */
+@Configuration
+public class Config extends WebMvcConfigurerAdapter {
+
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
+
+}
