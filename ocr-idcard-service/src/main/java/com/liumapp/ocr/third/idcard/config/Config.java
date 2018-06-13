ee41c74f44da916ed52cba9cd58d6f1ac8943623
liumapp <liumapp.com@gmail.com>
2018-06-13 14:20:33 +0800

    update package naem

--- ocr-idcard-service/src/main/java/com/liumapp/ocr/third/idcard/config/Config.java
+++ ocr-idcard-service/src/main/java/com/liumapp/ocr/third/idcard/config/Config.java
@@ -1,4 +1,4 @@
-package com.liumapp.ocr.third.idcard;
+package com.liumapp.ocr.third.idcard.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
